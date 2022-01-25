4a0324ba8b03b0f630363508e127ed02b4a32f90
Illia Pauliushchyk <illia.pauliushchyk@itechart-group.com>
2022-01-25 14:17:20 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
